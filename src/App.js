51a3a221bcac2d7a0f54943ab6ff6157cd247314
wageshwari <vaishnaviwageshwari@gmail.com>
2022-06-21 10:45:04 +0530

    remove Components file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component }  from 'react';
 import './App.css';
+import Counter from './components/counter.jsx';
 function App() {
   return (
-    <div className="App">
-      <h1 className='my-3'>hi babe, i love you babe.</h1>
-      <button className='btn btn-danger'>Love you Babe</button>
-    </div>
+   <App></App>
   );
 }
 
