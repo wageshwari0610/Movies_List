277d1ec05d430efa7f36455031afe37c59a29f48
wageshwari <vaishnaviwageshwari@gmail.com>
2022-07-13 22:38:03 +0530

    competed

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,21 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
 class Counter extends Component {
-  formatText(){
-    return this.props.counter.value===0?"btn btn-secondary":"btn btn-primary";
+  formatText() {
+    return this.props.counter.value === 0
+      ? "btn btn-secondary"
+      : "btn btn-primary";
   }
 
-  render() { 
+  render() {
     return (
-      <div >
+      <div>
         <span className={this.formatText()}>{this.props.counter.value}</span>
-        <button className="btn btn-success btn-lg m-2 " onClick={()=>this.props.onIncrement(this.props.counter)}>+</button>
-        <button className="btn btn-warning btn-lg m-2 " onClick={()=>this.props.onDecrement(this.props.counter)}>-</button>
-        <button className="btn btn-danger btn-lg m-2" onClick={()=>this.props.onDelete(this.props.counter.id)} >X</button>
+        <button
+          className="btn btn-success btn-lg m-2 "
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          
+        >
+          +
+        </button>
+        <button
+          className="btn btn-warning btn-lg m-2 "
+          onClick={() => this.props.onDecrement(this.props.counter)}
+          disabled={this.props.counter.value===0?"disabled":""}
+        >
+          -
+        </button>
+        <button
+          className="btn btn-danger btn-lg m-2"
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+        >
+          X
+        </button>
       </div>
     );
   }
 }
- 
+
 export default Counter;
